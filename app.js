8165df385375c82f29a6564c76f68ca1580e15c3
Felipe Ramos Braidotti <braidottifelipe@hotmail.com>
2023-05-03 20:58:57 -0300

    Adding HTML and CSS files to the preject

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-let x = 1;
+var maintenancePlan = {
+  groupOne: {
+    answerOne: document.getElementById("answerOne").value,
+    weightOne: 0.12,
+  },
+};
+
+function groupOneResult(a1, p1) {
+  if (a1 === "1") {
+    x1 = 1;
+  } else {
+    x1 = 0.5;
+  }
+  return x1 * p1;
+}
+
+function calcular() {
+  console.log(
+    groupOneResult(
+      maintenancePlan.groupOne.answerOne,
+      maintenancePlan.groupOne.weightOne
+    )
+  );
+  console.log(maintenancePlan.groupOne.answerOne);
+}
