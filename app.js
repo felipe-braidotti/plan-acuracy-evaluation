986e4d0c12a835b5139466f49a5706e2400bdfa1
Felipe Ramos Braidotti <braidottifelipe@hotmail.com>
2023-05-06 17:48:31 -0300

    Add second group and testing to perform the desired logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let weightOne = 0.12;
-let weightTwo = 0.08;
+let weightsGroupOne = [0.12, 0.08];
 let resultsGroupOne = [0, 0];
+let weightsGroupTwo = [0.1, 0.1];
+let resultsGroupTwo = [0, 0];
 
 function questionResult(answer, weight) {
   if (answer === "1") {
@@ -13,20 +14,54 @@ function questionResult(answer, weight) {
   return x1 * weight;
 }
 
-// document.getElementById("answerOne").addEventListener("select", calcular());
-
 function calculateQ1() {
   let answerOne = document.getElementById("answerOne").value;
-  let resultOne = questionResult(answerOne, weightOne);
+  let resultOne = questionResult(answerOne, weightsGroupOne[0]);
   resultsGroupOne[0] = resultOne;
   console.log(`Resultado da Questão 01: `, resultOne);
-  console.log(resultsGroupOne[0] + resultsGroupOne[1]);
+  console.log(
+    resultsGroupOne[0] +
+      resultsGroupOne[1] +
+      resultsGroupTwo[0] +
+      resultsGroupTwo[1]
+  );
 }
 
 function calculateQ2() {
   let answerTwo = document.getElementById("answerTwo").value;
-  let resultTwo = questionResult(answerTwo, weightTwo);
+  let resultTwo = questionResult(answerTwo, weightsGroupOne[1]);
   resultsGroupOne[1] = resultTwo;
   console.log(`Resultado da Questão 02: `, resultTwo);
-  console.log(resultsGroupOne[0] + resultsGroupOne[1]);
+  console.log(
+    resultsGroupOne[0] +
+      resultsGroupOne[1] +
+      resultsGroupTwo[0] +
+      resultsGroupTwo[1]
+  );
+}
+
+function calculateQ3() {
+  let answerThree = document.getElementById("answerThree").value;
+  let resultThree = questionResult(answerThree, weightsGroupTwo[0]);
+  resultsGroupTwo[0] = resultThree;
+  console.log(`Resultado da Questão 03: `, resultThree);
+  console.log(
+    resultsGroupOne[0] +
+      resultsGroupOne[1] +
+      resultsGroupTwo[0] +
+      resultsGroupTwo[1]
+  );
+}
+
+function calculateQ4() {
+  let answerFour = document.getElementById("answerFour").value;
+  let resultFour = questionResult(answerFour, weightsGroupTwo[1]);
+  resultsGroupTwo[1] = resultFour;
+  console.log(`Resultado da Questão 04: `, resultFour);
+  console.log(
+    resultsGroupOne[0] +
+      resultsGroupOne[1] +
+      resultsGroupTwo[0] +
+      resultsGroupTwo[1]
+  );
 }
